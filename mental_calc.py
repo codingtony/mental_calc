bdccb90f8b51b1683b0fdf2ada9d7930df0bcd37
Tony Bussieres <tony.bussieres@revelate.co>
2023-06-18 09:36:25 -0400

    Add colors and count mistakes

--- mental_calc.py
+++ mental_calc.py
@@ -1,6 +1,21 @@
 from datetime import datetime
-import random,os
-os.system('cls' if os.name == 'nt' else 'echo -e \\\\033c')
+import random, os
+
+os.system("cls" if os.name == "nt" else "echo -e \\\\033c")
+
+
+# The function and the class taken from https://stackoverflow.com/a/73917459/576794
+def color_text(text, rgb):
+    r, g, b = rgb
+    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
+
+class rgb:
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    YELLOW = (255, 255, 0)
+    WHITE = (255, 255, 255)
+
 
 motivation_quotes = [
     "Great work!",
@@ -20,7 +35,7 @@ motivation_quotes = [
     "Way to go!",
     "Good job!",
     "You're a rockstar!",
-    "You've got this!"
+    "You've got this!",
 ]
 
 wrong_answer_messages = [
@@ -28,42 +43,59 @@ wrong_answer_messages = [
     "Not quite, but keep trying!",
     "Almost there, but not quite.",
     "That's not the correct answer, but don't give up!",
-    "Sorry, that's incorrect. Try again!"
+    "Sorry, that's incorrect. Try again!",
 ]
 
-question_bank=[]
-# Generate all the possibilities for addition and substractions during 
+question_bank = []
+# Generate all the possibilities for addition and substractions during
 # manual additions and substractions (excluding when answer is 0)
 # or when one of the operand is zero
-for i in range(0,10):
-    for j in range(1,10):
-        if (i != 0): # generate the addition
-            o=f"{j} + {i}"
-            question_bank.append((o,j+i))
-        if i == j: # zero
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i != 0:  # generate the addition
+            o = f"{j} + {i}"
+            question_bank.append((o, j + i))
+        if i == j:  # zero
             continue
         if i > j and i != 0:
-            o=f"{i} - {j}"
-            question_bank.append((o,i-j))
+            o = f"{i} - {j}"
+            question_bank.append((o, i - j))
         else:
-            o=f"{i+10} - {j}"
-            question_bank.append((o,i+10-j))
+            o = f"{i+10} - {j}"
+            question_bank.append((o, i + 10 - j))
 
 start = datetime.now()
-number_questions=25
+number_questions = 5
+mistakes = 0
 
-for i in range(0,number_questions):
-    q=random.choice(question_bank)
+for i in range(0, number_questions):
+    q = random.choice(question_bank)
     while True:
-        a=input(f"{q[0]} = ")
+        a = input(color_text(f"{q[0]} = ", rgb.WHITE))
+        success = True
         try:
-            if (q[1] == int(a)):
-                print(random.choice(motivation_quotes)+'\n')
+            if q[1] == int(a):
+                print(color_text(random.choice(motivation_quotes) + "\n", rgb.GREEN))
                 break
             else:
-                print(random.choice(wrong_answer_messages)+'\n')
+                success = False
         except:
-            print(random.choice(wrong_answer_messages)+'\n')
-         
-duration=int(round((datetime.now() - start).total_seconds()))
-print(f"\nCongratulations!\nYou completed the test in {duration} seconds")
+            success = False
+        if not success:
+            mistakes += 1
+            print(color_text(random.choice(wrong_answer_messages) + "\n", rgb.RED))
+
+duration = int(round((datetime.now() - start).total_seconds()))
+print(
+    color_text(f"You completed the game in ", rgb.YELLOW)
+    + color_text(f"{duration} seconds", rgb.WHITE)
+)
+print(
+    color_text(f"Number of mistakes: ", rgb.YELLOW)
+    + color_text(f"{mistakes}\n", rgb.WHITE)
+)
+
+if mistakes / number_questions > 0.1 or duration / number_questions > 5:
+    print(color_text("You should practice more!\n", rgb.RED))
+else:
+    print(color_text("You scored well! Congratulations!\n", rgb.GREEN))
