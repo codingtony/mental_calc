40ebbb84846c659bac70e3103d589469b2f3e017
Tony Bussieres <tony.bussieres@revelate.co>
2023-06-30 07:01:31 -0400

    wip

--- game.js
+++ game.js
@@ -1,87 +1,83 @@
-class Game  {
-    constructor(name, numQuestions=50) {
-        this.name=name;
-        this.numQuestions=numQuestions;
-        this.initGame()
-    }
-    #motivation_quotes = [
-        "Great work!",
-        "Excellent!",
-        "Amazing!",
-        "Impressive!",
-        "You're a superstar!",
-        "Keep up the good work!",
-        "Well done!",
-        "Fantastic!",
-        "Brilliant!",
-        "Outstanding!",
-        "Incredible!",
-        "Terrific!",
-        "Superb!",
-        "Awesome!",
-        "Way to go!",
-        "Good job!",
-        "You're a rockstar!",
-        "You've got this!",
-    ]
-    
-    #wrong_answer_messages = [
-        "Oops, that's not quite right.",
-        "Not quite, but keep trying!",
-        "Almost there, but not quite.",
-        "That's not the correct answer, but don't give up!",
-        "Sorry, that's incorrect. Try again!",
-    ]
+class Game {
+  constructor(name, numQuestions = 50) {
+    this.name = name;
+    this.numQuestions = numQuestions;
+    this.initGame();
+  }
+  #motivation_quotes = [
+    "Great work!",
+    "Excellent!",
+    "Amazing!",
+    "Impressive!",
+    "You're a superstar!",
+    "Keep up the good work!",
+    "Well done!",
+    "Fantastic!",
+    "Brilliant!",
+    "Outstanding!",
+    "Incredible!",
+    "Terrific!",
+    "Superb!",
+    "Awesome!",
+    "Way to go!",
+    "Good job!",
+    "You're a rockstar!",
+    "You've got this!",
+  ];
 
-    #question_bank = []
-    
-    initGame() {
-        this.elapsedMs=0
-        this.score=0
-        this.mistakes=0
-        for (let i = 0; i < 10; i++) {
-            for (let j = 1; j < 10; j++) {
-              if (i !== 0) {
-                let o = j + " + " + i;
-                this.#question_bank.push([o, j + i]);
-              }
-              if (i === j) {
-                continue;
-              }
-              if (i > j && i !== 0) {
-                let o = i + " - " + j;
-                this.#question_bank.push([o, i - j]);
-              } else {
-                let o = (i + 10) + " - " + j;
-                this.#question_bank.push([o, i + 10 - j]);
-              }
-            }
-          }
-    }
+  #wrong_answer_messages = [
+    "Oops, that's not quite right.",
+    "Not quite, but keep trying!",
+    "Almost there, but not quite.",
+    "That's not the correct answer, but don't give up!",
+    "Sorry, that's incorrect. Try again!",
+  ];
 
-    getQuestionAnswer() {
-        let questions=[]
-        for (let i=0; i<this.numQuestions;i++) {
-            let randomIndex = Math.floor(Math.random() * this.#question_bank.length);
-            questions.push(this.#question_bank[randomIndex]);
+  #question_bank = [];
+
+  initGame() {
+    this.elapsedMs = 0;
+    this.score = 0;
+    this.mistakes = 0;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 1; j < 10; j++) {
+        if (i !== 0) {
+          let o = j + " + " + i;
+          this.#question_bank.push([o, j + i]);
         }
-        return questions
+        if (i === j) {
+          continue;
+        }
+        if (i > j && i !== 0) {
+          let o = i + " - " + j;
+          this.#question_bank.push([o, i - j]);
+        } else {
+          let o = i + 10 + " - " + j;
+          this.#question_bank.push([o, i + 10 - j]);
+        }
+      }
     }
-    start() {
-        this.startTime=Date.now()
+    let questions = [];
+    for (let i = 0; i < this.numQuestions; i++) {
+      let randomIndex = Math.floor(Math.random() * this.#question_bank.length);
+      questions.push(this.#question_bank[randomIndex]);
     }
-    end() {
-        const millis = Date.now()-this.startTime
-        const minutes = Math.floor(millis / 60000);
-        const seconds = ((millis % 60000) / 1000).toFixed(0);
-        this.elapsedMs = millis
-        this.elapsedTime = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-    }
+    this.questionsIt=questions.entries()
+  }
 
+  next() {
+    return this.questionsIt.next()
+  }
+
+
+  start() {
+    this.startTime = Date.now();
+  }
+  end() {
+    const millis = Date.now() - this.startTime;
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    this.elapsedMs = millis;
+    this.elapsedTime = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
 }
-
-const game = new Game("Sasha")
-console.log(game)
-game.start()
-console.log(game.getQuestionAnswer())
-game.end()
--- term.js
+++ term.js
@@ -1,59 +1,76 @@
-function initTerm(game) {
-    var term = new Terminal();
-    term.open(document.getElementById("terminal-div"));
-    
-    function colorText(r, g, b) {
-      return (text,reset=true) => {
-        str=`\x1B[38;2;${r};${g};${b}m${text}`;
-        if (reset) {
-            str+='\x1B[0m'
-        }
-        return str
-      };
-    }
-    
-    WHITE = colorText(255, 255, 255);
-    GREEN = colorText(0, 255, 0);
-    YELLOW = colorText(255, 255, 0);
-    RED = colorText(255, 0, 0);
-    
-    
-    function handleTerminal(controller) {
-      var buffer = [];
-      return (data) => {
-        for (let i = 0; i < data.length; ++i) {
-          const c = data[i];
-          if (c === "\r") {
-            term.write("\r\n");
-            controller.data(buffer);
-            buffer = [];
-            term.write(controller.prompt());
-          } else if (c === "\x7F") {
-            if (buffer.length > 0) {
-              term.write("\b \b");
-              buffer.pop();
-            }
-          } else {
-            term.write(c);
-            buffer.push(c);
-          }
-        }
-      };
-    }
-    
-    const controller = {
-       
-        initPrompt: () => {
-            return WHITE(`You will have to answer ${game.numQuestions} questions are you ready to start?`)+YELLOW("",false)
-        },
-        prompt: () => {
-           return GREEN("1 + 1 =") + " ";
-        },
-        data: (data) => {
-          console.log(data.join(""));
-        },
+function initTerm(controller) {
+  var term = new Terminal();
+  term.open(document.getElementById("terminal-div"));
+
+  function colorText(r, g, b) {
+    return (text, reset = true) => {
+      str = `\x1B[38;2;${r};${g};${b}m${text}`;
+      if (reset) {
+        str += "\x1B[0m";
       }
-    
-    term.onData(handleTerminal(controller));
-    term.write(controller.initPrompt())
+      return str;
+    };
+  }
+
+  WHITE = colorText(255, 255, 255);
+  GREEN = colorText(0, 255, 0);
+  YELLOW = colorText(255, 255, 0);
+  RED = colorText(255, 0, 0);
+
+  function handleTerminal(controller) {
+    var buffer = [];
+    return (data) => {
+      for (let i = 0; i < data.length; ++i) {
+        const c = data[i];
+        if (c === "\r") {
+          term.write("\r\n");
+          controller.data(buffer);
+          buffer = [];
+          term.write(controller.prompt());
+        } else if (c === "\x7F") {
+          if (buffer.length > 0) {
+            term.write("\b \b");
+            buffer.pop();
+          }
+        } else {
+          term.write(c);
+          buffer.push(c);
+        }
+      }
+    };
+  }
+
+
+
+  term.onData(handleTerminal(controller));
+  term.write(controller.initPrompt());
+}
+
+class Controller {
+  constructor(game) {
+    this.#game = game
+    console.log(game)
+    this.started=false
+    this.nextQuestion=null
+  }
+  #quetions = []
+  #game = null
+
+  initPrompt() {
+    return WHITE(
+      `You will have to answer ${this.#game.numQuestions} questions press enter to start`
+    );
+  }
+  prompt() {
+    return GREEN() + this.nextQuestion;
+  }
+  data(data) {
+    if (!this.started) {
+
+      this.started=true
+      this.nextQuestion=this.#game.next()
+      console.log("here "+this.nextQuestion)
+    }
+    console.log(data.join(""));
+  }
 }
