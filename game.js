ee117b9a47402b6b3cc1216f5592d18a4a5fdc2a
Tony Bussieres <tony.bussieres@revelate.co>
2023-06-30 19:11:52 -0400

    almost final version

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
+function msToString(millis) {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + " min " + (seconds < 10 ? "0" : "") + seconds + " sec";
+}
+
 class Game {
   constructor(name, numQuestions = 50) {
     this.name = name;
     this.numQuestions = numQuestions;
     this.initGame();
+    this.levelHighscore = [];
   }
   #firstTime = true;
 
@@ -40,6 +47,7 @@ class Game {
   initGame() {
     this.elapsedMs = 0;
     this.score = 0;
+    this.finish = false;
     this.mistakes = 0;
     for (let i = 0; i < 10; i++) {
       for (let j = 1; j < 10; j++) {
@@ -101,25 +109,81 @@ class Game {
   }
 
   #msToString(millis) {
-    const minutes = Math.floor(millis / 60000);
-    const seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + " min " + (seconds < 10 ? "0" : "") + seconds + " sec";
+    return msToString(millis);
   }
   secsToString(secs) {
-    return this.#msToString(1000*secs)
+    return this.#msToString(1000 * secs);
   }
 
   expectedTimeToCompletion() {
-    return this.numQuestions*5
+    return this.numQuestions * 5;
+  }
+
+  getHighscore() {
+    return this.levelHighscore;
   }
 
   hasWon() {
-    return (this.elapsedMs < this.expectedTimeToCompletion()*1000)
+    return (
+      this.elapsedMs < this.expectedTimeToCompletion() * 1000 &&
+      1 - this.mistakes / this.numQuestions > 0.9
+    );
   }
-  
+
   end() {
     const millis = Date.now() - this.startTime;
     this.elapsedMs = millis;
-    this.elapsedTime = this.#msToString(millis)
+    this.elapsedTime = this.#msToString(millis);
+    let highscore = localStorage.getItem("highscore");
+    if (highscore == null) {
+      highscore = {};
+    } else {
+      highscore = JSON.parse(highscore);
+    }
+    let levelHighscore = [];
+    let jsonLevelHighscore = highscore[this.numQuestions];
+    if (jsonLevelHighscore != null) {
+      jsonLevelHighscore.forEach((element) => {
+        levelHighscore.push(Object.assign(new Score(), element));
+      });
+    }
+    levelHighscore.push(
+      new Score(this.name, this.elapsedMs, this.numQuestions, this.mistakes)
+    );
+    highscore[this.numQuestions] = levelHighscore;
+    localStorage.setItem("highscore", JSON.stringify(highscore));
+    this.levelHighscore = levelHighscore;
+  }
+}
+
+class Score {
+  constructor(name, elapsedMs, numQuestions, mistakes) {
+    this.name = name;
+    this.elapsedMs = elapsedMs;
+    this.numQuestions = numQuestions;
+    this.mistakes = mistakes;
+
+    this.timestamp = Date.now();
+    this.valueOf = this.#valueOf;
+    this.elapsed = msToString(this.elapsedMs);
+  }
+
+  #valueOf() {
+    const spq = this.elapsedMs / 1000 / this.numQuestions;
+    const success = 1 - this.mistakes / this.numQuestions;
+    return ((this.numQuestions * 100) / spq) * success;
+  }
+
+  when() {
+    const date = new Date(this.timestamp)
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+    
+    return formattedDate;
   }
 }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   let nameEl = document.getElementById("name");
+  let numQuestionEl = document.getElementById("numQuestions");
   let terminalDiv = document.getElementById("terminal-div");
   let questionForm = document.getElementById("questions");
   let finish = document.getElementById("finish");
@@ -7,8 +8,12 @@ window.onload = function () {
 
   function initQuestions() {
     let username = localStorage.getItem("name");
+    let numQuestions = localStorage.getItem("numQuestions");
     if (username != null) {
       nameEl.value = username;
+    }
+    if (numQuestions != null) {
+      numQuestionEl.value = numQuestions;
     }
   }
 
@@ -16,15 +21,16 @@ window.onload = function () {
 
   againBtn.addEventListener("click", function () {
     finish.classList.add("hidden");
-    finish.classList.remove("maindiv");
+    finish.classList.remove("resultdiv");
     questionForm.classList.remove("hidden");
     initQuestions();
   });
   questionForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
     let username = nameEl.value;
-    let numQuestions = document.getElementById("numQuestions").value;
-    localStorage.setItem("name", nameEl.value);
+    let numQuestions = numQuestionEl.value;
+    localStorage.setItem("name", username);
+    localStorage.setItem("numQuestions", numQuestions);
     const game = new Game(username, numQuestions);
     questionForm.classList.add("hidden");
     terminalDiv.classList.remove("hidden");
@@ -32,43 +38,47 @@ window.onload = function () {
     function completionCallback() {
       terminalDiv.classList.add("hidden");
       finish.classList.remove("hidden");
-      finish.classList.add("maindiv");
+      finish.classList.add("resultdiv");
 
-      const leaderboardData = [
-        { name: "Player 1", mistakes: 2, time: "00:12" },
-        { name: "Player 2", mistakes: 1, time: "00:08" },
-        { name: "Player 3", mistakes: 5, time: "00:18" },
-        // ...
-        // Add more leaderboard entries here
-      ];
+      const leaderboardData = game.getHighscore();
       const leaderboardList = document.getElementById("leaderboard-list");
-
-      leaderboardData.forEach((entry, index) => {
-        if (index < 10) {
-          const listItem = document.createElement("li");
-          listItem.textContent = `${entry.name} - Mistakes: ${entry.mistakes} - Time: ${entry.time}`;
-          leaderboardList.appendChild(listItem);
-        }
-      });
+      const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour12: false,
+      };
+      leaderboardData
+        .sort((a, b) => {
+          return a.valueOf() - b.valueOf();
+        })
+        .reverse()
+        .forEach((entry, index) => {
+          if (index < 10) {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${entry.name} - Mistakes: ${
+              entry.mistakes
+            } - Time: ${entry.elapsed} - ${entry.when()}`;
+            leaderboardList.appendChild(listItem);
+          }
+        });
       const resultDiv = document.getElementById("result");
       resultDiv.innerHTML = "";
 
       const resultMessage = document.createElement("p");
       const statsMessage = document.createElement("p");
 
-      statsMessage.textContent = `${game.numQuestions} questions, ${game.mistakes} mistakes, ${game.elapsedTime}`
+      statsMessage.textContent = `${game.numQuestions} questions, ${game.mistakes} mistakes, ${game.elapsedTime}`;
 
       if (game.hasWon()) {
         resultMessage.textContent = "Congratulations! You did great!";
         resultMessage.classList.add("good");
-
       } else {
         resultMessage.textContent = "Oops! Better luck next time.";
         resultMessage.classList.add("bad");
       }
       resultDiv.appendChild(statsMessage);
       resultDiv.appendChild(resultMessage);
-      
     }
     initTerm(new Controller(game, completionCallback));
   });
